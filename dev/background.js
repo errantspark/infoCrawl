541e81c7643dbd75521c3e01eee1d1692f4061aa
Matt Vana <errantspark@gmail.com>
2013-09-17 10:19:35 -0700

    figuring out what everything does again

--- dev/background.js
+++ dev/background.js
@@ -1,4 +1,3 @@
-
 function check(tab_id, data, tab){
   if (tab.url.indexOf('wikipedia.org') > -1){
     chrome.pageAction.show(tab_id);
@@ -9,20 +8,27 @@ function check(tab_id, data, tab){
 
 chrome.tabs.onUpdated.addListener(check);
 
-var info;
+var info = [];
 
 var onbrowse = function(e){
+  console.log('l14');
   console.log(e);
+  var clos;
   chrome.tabs.get(e.tabId, function(t){
+    clos = t;
+    console.log('l17');
     console.log(t);
     console.log(t.title);
   });
-  info = e.url;
+  info.push([e,clos]);
+  var local;
+  chrome.storage.local.get(fuction(e){local = e});
 };
 
 chrome.webNavigation.onCompleted.addListener(onbrowse, {url: [{hostSuffix: 'wikipedia.org'}]});
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(ev){
+  console.log('l27');
   console.log(ev);
   chrome.tabs.getCurrent(function(ta){
     console.log(ta);
